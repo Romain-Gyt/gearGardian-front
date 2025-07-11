ec6c001ce10f32cbd85b3d147c50aaf6a6fd177e
rguyenot2023 <romain.guyenot2023@campus-eni.fr>
2025-07-11 16:44:40 +0200

    ajout route upload

--- src/lib/api/equipment.ts
+++ src/lib/api/equipment.ts
@@ -1,17 +1,17 @@
-// api.ts (client)
+
 import { api } from './client';
 import type { EPIRequestPayload, EPI} from '@/lib/types';
 
 
-// Récupère la liste des équipements de l'utilisateur authentifié
+
 export async function getEquipmentList() {
   const { data } = await api.get<EPI[]>('/epi/me');
   return data;
 }
 
-// Crée ou met à jour un équipement
+
 export async function saveEquipment(
-    equipment: EPIRequestPayload,  // on envoie exactement le payload attendu
+    equipment: EPIRequestPayload,
     id?: string,
 ) {
   if (id) {
